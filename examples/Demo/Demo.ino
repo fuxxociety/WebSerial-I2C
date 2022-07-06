9cfa8510cf04f173d3d1fbf0b1a48ccbd3fedec0
fuxxociety <fuxxociety@gmail.com>
2022-07-05 23:33:58 -0500

    Update Demo.ino
    Instead of WebSerial, perform I2C sniffing and pipe to WebSerial console

--- examples/Demo/Demo.ino
+++ examples/Demo/Demo.ino
@@ -20,22 +20,54 @@
 
 AsyncWebServer server(80);
 
+/* STA mode */
 const char* ssid = ""; // Your WiFi SSID
 const char* password = ""; // Your WiFi Password
 
+/* I2C Pin Assignments */
+const char sda_pin = D2;
+const char scl_pin = D1;
+
+bool buf[4096];
+bool* bufI = &buf[0];
+const uint32_t PAUSE = 1 << 20;
 
 /* Message callback of WebSerial */
-void recvMsg(uint8_t *data, size_t len){
-  WebSerial.println("Received Data...");
-  String d = "";
-  for(int i=0; i < len; i++){
-    d += char(data[i]);
-  }
-  WebSerial.println(d);
+#void recvMsg(uint8_t *data, size_t len){
+#  WebSerial.println("Received Data...");
+#  String d = "";
+#  for(int i=0; i < len; i++){
+#    d += char(data[i]);
+#  }
+#  WebSerial.println(d);
+#}
+
+/* Message callback of I2C Bus to WebSerial */
+uint32_t waitCl(bool expected) {
+    uint32_t i = 0;
+    while (digitalRead(scl_pin) != expected) {
+      while ((digitalRead(scl_pin) != expected) && (++i % PAUSE != 0)) ;
+
+      if (i % PAUSE == 0) {
+        uint32_t i = 0;
+        while (&buf[i] != bufI) {
+          WebSerial.print(buf[i++]);
+          if (i % 9 == 0)
+            WebSerial.print(' ');
+        }
+        if (bufI != &buf[0])
+          WebSerial.print('\n');
+        bufI = &buf[0];
+        yield();
+      }
+    }
+    return i;
 }
 
 void setup() {
     Serial.begin(115200);
+    pinMode(sda_pin, INPUT);
+    pinMode(scl_pin, INPUT);
     WiFi.mode(WIFI_STA);
     WiFi.begin(ssid, password);
     if (WiFi.waitForConnectResult() != WL_CONNECTED) {
@@ -48,7 +80,13 @@ void setup() {
     WebSerial.begin(&server);
     /* Attach Message Callback */
     WebSerial.msgCallback(recvMsg);
-    server.begin();
+#    server.begin();
+    ## Instead of running the WebSerial monitor, do main call for I2C sniffing ##
+    while (true) {
+      uint32_t incr = waitCl(HIGH);
+      *bufI++ = digitalRead(sda_pin);
+      waitCl(LOW);
+    }
 }
 
 void loop() {
